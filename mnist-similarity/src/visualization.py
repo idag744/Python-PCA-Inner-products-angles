dfb051f8d0b59666b062747f93295fac4dba251f
Aida-Garfami <131345163+idag744@users.noreply.github.com>
2025-09-13 11:01:08 +0200

    Update visualization.py

--- mnist-similarity/src/visualization.py
+++ mnist-similarity/src/visualization.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_similarity_matrix(matrix, title: str, labels: list):
+    """Plot similarity (distance or angle) matrix as heatmap."""
+    fig, ax = plt.subplots(figsize=(6, 5))
+    grid = ax.imshow(matrix, interpolation='nearest', cmap="viridis")
+    ax.set(title=title, xticks=range(len(labels)), yticks=range(len(labels)))
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels(labels)
+    fig.colorbar(grid)
+    plt.show()
 
